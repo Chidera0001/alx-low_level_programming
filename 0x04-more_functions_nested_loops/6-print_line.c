20d67a8668eefccc6db10669b7c345d95ad4c4db
Chidera0001 <anelechidera4@gmail.com>
2022-09-16 01:17:21 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,19 +4,19 @@
  * print_line - print a straight line
  *
  * @n: is the number of times the _ character
- *  should be printed
+ *   should be printed
  */
 
 void print_line(int n)
 {
-	int lnChr;
+		int lnChr;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		for (lnChr = 1; lnChr <= n; ++lnChr)
-		      putchar(");
-		_putchar('\n');
-	}
-}
+		if (n <= 0)
+			_putchar('\n');
+		else
+		{
+			for (lnChr = 1; lnChr <= n; ++lnChr)
+				putchar('');
+			_putchar('\n');
+		}
+}		
