31b8acf3ade80d212b774a7298bc0d8b0fd073f3
Chidera0001 <anelechidera4@gmail.com>
2022-09-22 12:56:03 -0700

    TASKS

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the x
- *       null byte, to the end of the string pointed to by @dest
+ *       null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
@@ -10,7 +10,7 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int dex = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
